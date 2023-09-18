4cf437c9c7cc0fcf2c97ee9eed0b194ec471a81c
rajachaymae <rajafamilly633@gmail.com>
2023-09-18 10:00:41 -0700

    0

---
+++ README.md
@@ -0,0 +1,18 @@
+0Write a function that produces output according to a format.
+1Handle the following conversion specifiers
+2With a face like mine, I do better in print
+3 Handle the following conversion specifiers
+4Use a local buffer of 1024 chars in order to call write as little as possible.
+5Handle the following custom conversion specifier
+6 Handle the following conversion specifier: p.
+7Handle the following flag characters for non-custom conversion specifiers:
+8Handle the following length modifiers for non-custom conversion specifiers:
+9Handle the field width for non-custom conversion specifiers.
+10Handle the precision for non-custom conversion specifiers.
+11Handle the 0 flag character for non-custom conversion specifiers.
+12 Handle the - flag character for non-custom conversion specifiers.
+13Handle the following custom conversion specifier:
+14Handle the following custom conversion specifier:
+
+R: prints the rot13'ed string
+15 All the above options work well together.
---
+++ _printf.c
@@ -0,0 +1,70 @@
+#include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
+
+/**
+ * _printf - function that produces output according to a format.
+ * @format:  is a character string.
+ * Return:  the number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	if (format == NULL)
+		return (-1);
+
+	int printfeed = 0;
+	int print_char = 0;
+	int i = 0;
+	int flags, width, precision, size, buff_indicator = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
+
+	va_start(list, format);
+
+	while (format && format[i] != '\0')
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_indicator++] = format[i];
+			if (buff_indicator == BUFF_SIZE)
+				print_buffer(buffer, &buff_indicator);
+			/* write(1, &format[i], 1);*/
+			print_char++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_indicator);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printfeed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printfeed == -1)
+				return (-1);
+			print_char += printfeed;
+		}
+		i++;
+	}
+
+	print_buffer(buffer, &buff_indicator);
+
+	va_end(list);
+
+	return (print_char);
+}
+
+/**
+ * print_buffer -here we  Prints, in case of existance the buffer, the contents
+ * @buffer: define an array of chars types
+ * @buff_indicator: represents the length.
+ */
+void print_buffer(char buffer[], int *buff_indicator)
+{
+	if (*buff_indicator > 0 && buff_indicator != '\0')
+		write(1, &buffer[0], *buff_indicator);
+
+	*buff_indicator = 0;
+}
+
